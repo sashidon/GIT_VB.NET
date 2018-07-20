b7f0c36519b80cb0a2e76729a1979887e6b7fa78
Sashi_SK <sashidon10@gmail.com>
2018-07-20 09:19:22 +0800

    second commite two line

--- GIT_VB.NET/Form1.vb
+++ GIT_VB.NET/Form1.vb
@@ -3,5 +3,7 @@
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
         Dim name As String
         name = "1 st commite"
+        Dim age As String
+        age = 24
     End Sub
 End Class
