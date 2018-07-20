ead91d1c7dfec1b64c868062b92e99bb52b78e38
Sashi_SK <sashidon10@gmail.com>
2018-07-20 09:08:24 +0800

    start point one line only

---
+++ GIT_VB.NET/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim name As String
+        name = "1 st commite"
+    End Sub
+End Class
